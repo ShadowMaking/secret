ecd768d7b27417121a6501a7ae795f380d2ac9ea
IeigenHaimi <skyeGaolei666@ieigen.com>
2021-08-23 21:32:44 +0800

    [feat-metamask]-chore: optimize vue config

--- vue.config.js
+++ vue.config.js
@@ -101,8 +101,8 @@ module.exports = {
       config.externals = {
         // 'vue': 'Vue',
         // 'vue-router': 'VueRouter',
-        'moment': 'moment',
-        'lodash': 'lodash',
+        // 'moment': 'moment',
+        // 'lodash': 'lodash',
        }
     } else {
       config.mode = "development";
