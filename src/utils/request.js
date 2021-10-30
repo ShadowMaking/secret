aec5f13465e18fd936c7f55ffcbd366c80726682
IeigenHaimi <skyeGaolei666@ieigen.com>
2021-10-31 00:43:26 +0800

    [social-recovery]-fix: error is 1 need login

--- src/utils/request.js
+++ src/utils/request.js
@@ -59,7 +59,7 @@ axios.defaults.withCredentials = true;
          case 1: // need login
           const userId = getFromStorage('gUID')
           const token = getAuthToken('gtoken')
-          const url = `${window.location.protocol}//secret.ieigen.com?id=${userId}&auth_token=${token}`
+          const url = `${window.location.protocol}//secret.ieigen.com`
           window.location.href=url
           return Promise.reject('error')
          default:
