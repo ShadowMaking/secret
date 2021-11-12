fd5bab19776baa9e8642d9363eacf2d7a42244bc
rita <1183624592@qq.com>
2021-11-12 22:31:09 +0800

    fix introduction

--- src/views/Introduction/index.vue
+++ src/views/Introduction/index.vue
@@ -7,12 +7,13 @@
       </div>
       <span class="tip">Social Recovery is a secret recovery mechanism, allowing you to place your secret or private data in the custody of your friends by Cryptography Threshold Secret Share and multi-sig contract wallet on Layer 2. And the multi-sig wallet will be available in early December.
       </span>
-      <span class="tip">Important: all your keys and shares are processed at your computer or device, Eigrn team can't access them. </span>
+      <span class="tip" style="color:red">Important: all your keys and shares are processed at your computer or device, Eigrn team can't access them. </span>
       <span class="tip">Now you can:</span>
-      <span class="tip">Create Secret:</span>
-      <span class="tip">Generate your private key or key recovery phrases randomly, split and send the shares to your friends' email;</span>
-      <span class="tip">Recover Secret：</span>
-      <span class="tip">Collect the shares from your friends, and recover your original secret.</span>
+      <ul>
+        <li>Create Secret: Generate your private key or key recovery phrases randomly, split and send the shares to your friends' email.</li>
+        <li>Import Secret： Import your existing secret to create a recovery, split and share it to your friends' email.</li>
+        <li>Recover Secret：Collect the shares from your friends, and recover your original secret.</li>
+      </ul>
     </div>
     <div class="tip-wrapper bridge">
       <div class="top">
