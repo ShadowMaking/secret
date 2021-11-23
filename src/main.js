ff2154bf1df3d12ac3472f319ed6304898539d37
IeigenHaimi <skyeGaolei666@ieigen.com>
2021-11-23 08:49:17 +0800

    [feature-dashboard]-chore: add echarts

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import { initWeb3 } from "@/utils/web3";
+import * as echarts from 'echarts';
 
 Vue.config.productionTip = false;
 Vue.prototype.web3 = initWeb3();
 Vue.prototype.$eventBus = new Vue();
+Vue.prototype.$echarts = echarts;
 
 new Vue({
   router,
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -3,6 +3,9 @@
     <van-button @click="contract">contract</van-button>
     <van-button @click="getInfo">getInfo</van-button>
     <van-button @click="send">Send</van-button>
+    <div id="app">
+      <div id="main" style="width: 100%; height:400px; border: 1px solid red"></div>
+    </div>
   </div>
 </template>
 <script>
@@ -51,8 +54,8 @@ export default {
       console.log(info)
     },
     async send() {
-      // const tokenAddress = '0x32E6C34Cd57087aBBD59B5A4AECC4cB495924356'
-      const tokenAddress = '0xe4815AE53B124e7263F08dcDBBB757d41Ed658c6'
+      const tokenAddress = '0x3fa400483487A489EC9b1dB29C4129063EEC4654'
+      // const tokenAddress = '0xe4815AE53B124e7263F08dcDBBB757d41Ed658c6' // zks
       const toAddress = '0xDCc1614667ECF280cb2938405f339bFbC3Ab833D';
       const url = `https://api.etherscan.io/api?module=contract&action=getabi&address=${tokenAddress}&apikey=YourApiKeyToken`
       const ajaxObj = new XMLHttpRequest();
@@ -85,6 +88,36 @@ export default {
         }
       }
     },
+    drawChart() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById("main"));
+      // 指定图表的配置项和数据
+      let option = {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        legend: {
+          data: ["销量"]
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    },
   },
+  mounted() {
+    this.drawChart();
+  }
 }
 </script>
