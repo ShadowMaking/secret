b5d12549a9908eb0f15a304a135b2561230a14bc
rita <1183624592@qq.com>
2022-02-16 17:04:59 +0800

    input back

--- src/components/Input/index.vue
+++ src/components/Input/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="input-page">
-  	<div class="input-label">{{label}}</div>
-  	<div class="input-con">
-  	  <input
+    <div class="input-label">{{label}}</div>
+    <div class="input-con">
+      <input
         @input="inputChange"
-  	    type="text"
+        type="text"
         :value="value" 
-  	    class="input-text" 
-  	    :style="{paddingLeft: (leftIcon ? (25+'px') : (0 + 'px'))}"
-  	    :placeholder="placeholder ? placeholder : ''" 
+        class="input-text" 
+        :style="{paddingLeft: (leftIcon ? (25+'px') : (0 + 'px'))}"
+        :placeholder="placeholder ? placeholder : ''" 
         @keyup="hanldeValue"
-  	  >
-  	  <img :src="leftIcon" class="input-value-icon" v-if="leftIcon">
-  	  <div class="input-right" v-if="rightIcon">
-  	  	<img :src="rightIcon" class="input-right-icon">
-  	  </div>
-  	</div>
+      >
+      <img :src="leftIcon" class="input-value-icon" v-if="leftIcon">
+      <div class="input-right" v-if="rightIcon">
+        <img :src="rightIcon" class="input-right-icon">
+      </div>
+    </div>
   </div>
   
 </template>
@@ -23,26 +23,13 @@
 <script>
 import Vue from 'vue';
 import { Field } from 'vant';
-import { getInfoFromStorageByKey } from '@/utils/storage';
-
 Vue.use(Field);
-
 export default {
   name: 'Input',
-  props: ['label', 'leftIcon', 'rightIcon', 'placeholder', 'value', 'limitInput', 'isSearch'],
-  data() {
-    return {
-      searchUserList: [],
-    }
-  },
+  props: ['label', 'leftIcon', 'rightIcon', 'placeholder', 'value', 'limitInput'],
   methods: {
     inputChange(e) {
       const value = e.target.value
-      if (this.isSearch && value.indexOf("@gmail.com") > -1) {
-        this.searchUserByEmail(value)
-      } else {
-        this.searchUserList = []
-      }
       this.$emit('inputChange',{ value });
     },
     hanldeValue(e) {
@@ -52,15 +39,6 @@ export default {
         this.value = value
         this.$emit('inputChange',{ value });
       }
-    },
-    async searchUserByEmail(value) {
-      var searchData = {
-        userId: getInfoFromStorageByKey('gUID'),
-        value: value
-      }
-      const { hasError, list } = await this.$store.dispatch('searchSigner', searchData);
-      this.searchUserList = list
-      console.log(this.searchUserList)
     },
   },
 };
